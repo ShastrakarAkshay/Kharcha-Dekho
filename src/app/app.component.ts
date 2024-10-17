bdc0a11323b6a5b912b187525bab7616f8160098
akshay.shastrakar <akshay.shastrakar@pubmatic.com>
2024-10-17 09:52:42 +0530

    firebase config added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +11,22 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterModule],
 })
-export class AppComponent {
-  title = 'expense_tracker';
+export class AppComponent implements OnInit {
+  title = 'kharcha dekho';
+
+  ngOnInit() {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyBCewB9DeINEAHb4xI7V8Rezzmb1HQtv2w',
+      authDomain: 'kharcha-dekho.firebaseapp.com',
+      projectId: 'kharcha-dekho',
+      storageBucket: 'kharcha-dekho.appspot.com',
+      messagingSenderId: '479825737854',
+      appId: '1:479825737854:web:56f8e06cea57b17486002f',
+      measurementId: 'G-C6WG9Y9Y41',
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+  }
 }
