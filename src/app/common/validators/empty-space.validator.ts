9b57de3fd42d387333926b7cddffa2bc154be6f7
akshay.shastrakar <akshay.shastrakar@pubmatic.com>
2024-11-01 21:29:25 +0530

    icon and validator updated

--- src/app/common/validators/empty-space.validator.ts
+++ src/app/common/validators/empty-space.validator.ts
@@ -2,7 +2,10 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 export function emptySpaceValidator(): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
-    const isEmptySpace = (control.value || '').trim().length === 0;
-    return isEmptySpace ? { emptySpace: true } : null;
+    if (control.value) {
+      const isEmptySpace = (control.value || '').trim().length === 0;
+      return isEmptySpace ? { emptySpace: true } : null;
+    }
+    return null;
   };
 }
--- src/app/core/profile/profile.constant.ts
+++ src/app/core/profile/profile.constant.ts
@@ -1,12 +1,12 @@
 import { ILinks } from './profile.interface';
 
 export const Links: ILinks[] = [
-  { id: 1, label: 'Edit Profile', path: '', icon: 'person' },
+  { id: 1, label: 'Edit Profile', path: '', icon: 'account_circle' },
   {
     id: 2,
     label: 'All Transactions',
     path: 'core/all-transactions',
-    icon: 'person',
+    icon: 'swap_horiz',
   },
-  { id: 3, label: 'Icons', path: 'core/icons', icon: 'person' },
+  { id: 3, label: 'Icons', path: 'core/icons', icon: 'interests' },
 ];
