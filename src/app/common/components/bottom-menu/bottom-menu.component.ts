1df7012d8c6b5bbf681e444fdaf7f39e98bb07cc
akshay.shastrakar <akshay.shastrakar@pubmatic.com>
2024-11-02 18:01:34 +0530

    chagne added

--- src/app/common/components/bottom-menu/bottom-menu.component.ts
+++ src/app/common/components/bottom-menu/bottom-menu.component.ts
@@ -15,23 +15,30 @@ export class BottomMenuComponent {
   constructor(private _router: Router) {}
   menus = [
     {
+      id: 1,
       icon: 'home',
       label: 'Home',
       path: 'core/dashboard',
+      active: false,
     },
     {
+      id: 2,
       icon: 'category',
       label: 'Category',
       path: 'core/category',
+      active: false,
     },
     {
+      id: 3,
       icon: 'person',
       label: 'Account',
       path: 'core/profile',
+      active: false,
     },
   ];
 
   navigate(menu: any) {
+    this.menus.forEach((x) => (x.active = x.id === menu.id));
     this._router.navigate([menu.path]);
   }
 }
