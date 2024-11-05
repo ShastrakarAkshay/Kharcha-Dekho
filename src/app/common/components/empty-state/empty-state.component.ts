0c593a11ef0b7afb3dbce48147c3e05151b8c4a9
akshay.shastrakar <akshay.shastrakar@pubmatic.com>
2024-11-05 17:49:35 +0530

    empty state updated

--- src/app/common/components/empty-state/empty-state.component.ts
+++ src/app/common/components/empty-state/empty-state.component.ts
@@ -10,4 +10,5 @@ import { Component, Input } from '@angular/core';
 export class EmptyStateComponent {
   @Input() message: string = '';
   @Input() subText: string = '';
+  @Input() height: string = '';
 }
