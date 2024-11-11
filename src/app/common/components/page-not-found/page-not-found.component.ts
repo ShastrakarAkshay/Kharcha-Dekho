cf144e0fc3120e1db2f49ed4378d4190151c196d
akshay.shastrakar <akshay.shastrakar@pubmatic.com>
2024-11-11 09:45:30 +0530

    page not found udpated

--- src/app/common/components/page-not-found/page-not-found.component.ts
+++ src/app/common/components/page-not-found/page-not-found.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './page-not-found.component.html',
-  styleUrl: './page-not-found.component.scss'
+  styleUrl: './page-not-found.component.scss',
 })
 export class PageNotFoundComponent {
+  constructor(private _router: Router) {}
 
+  goToHome() {
+    this._router.navigate(['core/dashboard']);
+  }
 }
